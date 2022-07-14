54da8817a57ca4759e564f0ec9836639b3db35a4
安辉 <privateworkemail@163.com>
2022-07-15 02:23:59 +0800

    支持打war包

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ https://www.imooc.com/video/23471
 - 1.把项目下sql/db_stu.sql导入到你本地的mysql数据库，
 
 - 2.修改application.yml文件中 datasource下的数据库相关信息,修改成你的.
+
+### 打包war包，运行到tomcat
+spring boot默认是有集成tomcat的，但是有可能需要打包war部署到服务器。
+参考链接:https://blog.51cto.com/u_15345018/3654011
+
+- 如果打包war如果运行访问404，尝试tomcat官网下载跟内嵌一样版本号的tomcat。
--- src/main/java/com/imooc/imoocspringbootstarter/StarterApplication.java
+++ src/main/java/com/imooc/imoocspringbootstarter/StarterApplication.java
@@ -3,11 +3,19 @@ package com.imooc.imoocspringbootstarter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 @MapperScan("com.imooc.imoocspringbootstarter.dao") //扫描dao接口
-public class StarterApplication {
+@SpringBootApplication
+public class StarterApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(StarterApplication.class, args);
+    }
+
+    // 按照下面的方式重写
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StarterApplication.class);
     }
 }
